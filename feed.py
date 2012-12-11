74afbb0dced2b29830667170c3f09970987e3ba1
Robert Kaye <rob@musicbrainz.org>
2012-12-11 13:14:35 -0800

    Move to use YAML for storing stuff on disk

--- feed.py
+++ feed.py
@@ -9,16 +9,17 @@ import yaml
 import tempfile
 import tarfile
 import shutil
+import subprocess
 from config import PG_CONNECT, PACKAGE, VERSION
 from changed_ids import get_changed_ids
 
-def read_state_data(filename):
+def read_state_data(data_dir, filename):
     """Read the last replication sequence and date from a YAML file and return as a tuple"""
 
+    full_path = os.path.join(data_dir, filename)
     try:
-        f = open(filename, "r")
+        f = open(full_path, "r")
     except IOError, e:
-        sys.stderr.write("Warning: cannot open file %s: %s\n" % (filename, e))
         return (None, None)
 
     try:
@@ -32,21 +33,22 @@ def read_state_data(filename):
     return (int(data['replication_sequence']), data['timestamp'])
             
 
-def save_state_data(filename, sequence, timestamp):
+def save_state_data(data_dir, filename, sequence, timestamp):
     """Write last replication sequence and data to a YAML file. return True/False"""
 
     try:
-        os.makedirs(DATA_FEED_DIR)
+        os.makedirs(data_dir)
     except os.error, e:
         if e.errno != 17: # dir exists
-            sys.stderr.write("Error: cannot create data dir %s: %s\n" % (DATA_FEED_DIR, str(e)))
-            sys.exit(-1)
+            print "Error: cannot create data dir %s: %s\n" % (dat_dir, str(e))
+            return False
 
+    full_path = os.path.join(data_dir, filename)
     try:
-        f = open(filename, "w")
+        f = open(full_path, "w")
     except IOError, e:
-        sys.stderr.write("Warning: cannot open last date file %s: %s. creating new file.\n" % (filename, e))
-        sys.exit(-1)
+        print "Warning: cannot write last date file %s: %s" % (full_path, e)
+        return False
 
     data = { 'replication_sequence' : sequence, 'timestamp' : timestamp }
     try:
@@ -68,13 +70,13 @@ def get_timestamp_from_replication_packet(sequence):
         response = opener.open(packet)
     except urllib2.HTTPError, err:
         if err.code == 404:
-            print "Replication packet not available."
+            print "Replication packet %d not available." % sequence
             return None
         print err.code
         return None
     except urllib2.URLError, err:
         if err.reason.find("550"):
-            print "Replication packet not available."
+            print "Replication packet %d not available." % sequence
             return None
         print err.reason
         return None
@@ -95,16 +97,16 @@ def get_timestamp_from_replication_packet(sequence):
 
     return timestamp.strip()
 
-def save_data(sequence, timestamp, data):
+def save_data(data_dir, sequence, timestamp, data):
     '''Save the data file. If its not written for whatever reason, don't return. die.'''
     try:
-        os.makedirs(OUTPUT_DIR)
+        os.makedirs(data_dir)
     except os.error, e:
         if e.errno != 17: # dir exists
-            sys.stderr.write("Error: cannot create data dir %s: %s\n" % (OUTPUT_DIR, str(e)))
+            sys.stderr.write("Error: cannot create data dir %s: %s\n" % (data_dir, str(e)))
             sys.exit(-1)
 
-    filename = os.path.join(OUTPUT_DIR, "changed-ids-%d.json" % sequence)
+    filename = os.path.join(data_dir, "changed-ids-%d.json" % sequence)
     try:
         f = open(filename, "w")
     except os.error, e:
@@ -129,9 +131,8 @@ def save_data(sequence, timestamp, data):
         os.unlink(filename)
         sys.exit(-1)
 
-def generate_data_feed():
+def generate_entry(data_dir, last_sequence, last_timestamp):
 
-    last_date = get_last_date()
     try:
         conn = psycopg2.connect(PG_CONNECT)
     except psycopg2.OperationalError as err:
@@ -141,19 +142,14 @@ def generate_data_feed():
     cur = conn.cursor()
     cur.execute("select current_replication_sequence, last_replication_date from replication_control");
     row = cur.fetchone()
-    rep_seq = row[0]
-    current_date = str(row[1])
+    current_sequence = row[0]
+    current_timestamp = str(row[1])
 
-    print "old date: %s\nnew date: %s\n" % (last_date, current_date)
-    if current_date == last_date: 
-        print "Dates are the same, no new data to process."
+    print "   last: %d at %s\ncurrent: %s at %s\n" % (last_sequence, last_timestamp, current_sequence, current_timestamp)
+    if last_sequence == current_sequence:
+        print "Replication sequence unchanged, no new data to process."
         return None
 
-    if last_date:
-        data = get_changed_ids(last_date, current_date)
-        data = json.dumps({ 'data' : data }) #, sort_keys=True, indent=4)
-        save_data(rep_seq, current_date, data)
-    else:
-        print "No last date available. Saving current date and doing fuck-all."
-
-    save_last_date(current_date)
+    data = get_changed_ids(last_timestamp, current_timestamp)
+    data = json.dumps({ 'data' : data }) #, sort_keys=True, indent=4)
+    save_data(data_dir, current_sequence, current_timestamp, data)
